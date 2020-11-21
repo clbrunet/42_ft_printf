f2d9ce262f7b600e4ebc1fc5d07fca6fdd71ba4b
clbrunet <clbrunet@student.42.fr>
2020-11-21 13:34:12 +0100

    save

--- main.c
+++ main.c
@@ -4,37 +4,40 @@
 
 int		main(void)
 {
+	/* TODO hu hx hhu hhx */
 	int res;
-	char	nothing1[100]	= "|%i| ";
-	char	width2[100]		= "|%25.10i| ";
-	char	precision3[100]	= "|%3.50i| ";
-	char	same4[100]		= "|%25.25i| ";
-	char	test5[100]		= "|%.2i| ";
-	int f = -236;
+	char	nothing1[100]	= "|%hhu| ";
+	char	width2[100]		= "|%-25.10hhu| ";
+	char	precision3[100]	= "|%-3.50hhu| ";
+	char	same4[100]		= "|%-25.25hhu| ";
+	char	test5[100]		= "|%hhu| ";
+	unsigned char u = 255;
 
+	/* printf("|%.hhx|\n", x); */
+	/* ft_printf("|%.hhx|\n", x); */
 	printf("----- TEST 1 -----\n");
-	res = printf(nothing1, f);
+	res = printf(nothing1, u);
 	printf("%i\n", res);
-	res = ft_printf(nothing1, f);
+	res = ft_printf(nothing1, u);
 	printf("%i\n", res);
 	printf("----- TEST 2 -----\n");
-	res = printf(width2, f);
+	res = printf(width2, u);
 	printf("%i\n", res);
-	res = ft_printf(width2, f);
+	res = ft_printf(width2, u);
 	printf("%i\n", res);
 	printf("----- TEST 3 -----\n");
-	res = printf(precision3, f);
+	res = printf(precision3, u);
 	printf("%i\n", res);
-	res = ft_printf(precision3, f);
+	res = ft_printf(precision3, u);
 	printf("%i\n", res);
 	printf("----- TEST 4 -----\n");
-	res = printf(same4, f);
+	res = printf(same4, u);
 	printf("%i\n", res);
-	res = ft_printf(same4, f);
+	res = ft_printf(same4, u);
 	printf("%i\n", res);
 	printf("----- TEST 5 -----\n");
-	res = printf(test5, f);
+	res = printf(test5, u);
 	printf("%i\n", res);
-	res = ft_printf(test5, f);
+	res = ft_printf(test5, u);
 	printf("%i\n", res);
 }
